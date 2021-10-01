838f65382dfeb4eba658075123d781d87b2d601d
anthonyc2394 <91750514+anthonyc2394@users.noreply.github.com>
2021-10-01 12:53:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Anthony"; // update the part BETWEEN the "quotes"
+let userName = "anthonyc2394"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
